b131fcd4e711110f4284a1efb427cfa90e39e6e6
MAR-Industries <xxdarksiderxx99@gmail.com>
2020-04-08 13:55:36 -0700

    update of test_room.fbx
    Scaled the room and added a temp object patrol script, for patrolling certain routes

---
+++ CS-179/Assets/Scripts/object patrol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1behavior : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speed = 5f;
+
+    Animator def_animator;
+    void Start()
+    {
+        def_animator = gameObject.GetComponent<Animator>();
+
+        StartCoroutine(Patrol());
+    }
+    IEnumerator Patrol()
+    {
+        while (true)
+        {
+            for (int w = 0; w < waypoints.Length; w++)
+            {
+                Transform thisWaypoint = waypoints[w];
+                Transform nextWaypoint;
+                if (w + 1 < waypoints.Length) nextWaypoint = waypoints[w + 1];
+                else nextWaypoint = waypoints[0];
+
+                float moveTime = (nextWaypoint.position - thisWaypoint.position).magnitude;
+                for (float t = 0f; t < moveTime; t += Time.deltaTime)
+                {
+                    transform.position = Vector3.Lerp(thisWaypoint.position, nextWaypoint.position, t/ moveTime * speed
+                        );
+                    yield return 0;
+                }
+                transform.position = nextWaypoint.position;
+                transform.LookAt(nextWaypoint.position);
+                yield return new WaitForSeconds(1f); //pause when you reach the target
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+           
+
+
+
+        
+    }
+}
