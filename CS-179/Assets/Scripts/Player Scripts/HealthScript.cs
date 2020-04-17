7b568f32942fbd3dd45407ba43347dfa152b51aa
JacquesFr <43708007+JacquesFr@users.noreply.github.com>
2020-04-16 18:05:04 -0700

    FPS Prefab Created

---
+++ CS-179/Assets/Scripts/Player Scripts/HealthScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HealthScript : MonoBehaviour
+{
+    //private EnemyAnimator enemy_animator;
+    private NavMeshAgent nav_agent;
+    //private EnemyController enemy_controller;
+
+    public float health = 100f;
+    public bool is_player;
+    private bool is_dead;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        /*
+        if(Enemy){
+            enemy_animator = GetComponent<EnemyAnimator>();
+            enemy_controller = GetComponent<EnemyController>();
+            nav_agent = GetComponent<NavMeshAgent>();
+
+        //get enemy audio
+        }
+
+        if(is_player){
+
+        }
+        */
+    }
+
+    public void ApplyDamage(float damage)
+    {
+
+        // if we died don't execute the rest of the code
+        if (is_dead)
+            return;
+
+        health -= damage;
+
+        if (is_player)
+        {
+            // show the stats(display the health UI value)
+            //player_Stats.Display_HealthStats(health);
+        }
+        /*
+        if (Enemy)
+        {
+            if (enemy_Controller.Enemy_State == EnemyState.PATROL)
+            {
+                enemy_Controller.chase_Distance = 50f;
+            }
+        }
+        */
+
+        if (health <= 0f)
+        {
+
+            //PlayerDied();
+
+            is_dead = true;
+        }
+
+    } // apply damage
+}
---
+++ CS-179/Assets/Scripts/Player Scripts/Weapons Script/WeaponManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField]
+    private WeaponHandler[] weapons;
+
+    private int current_weapon_index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_weapon_index = 0;
+        weapons[current_weapon_index].gameObject.SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            turnOnSelectedWeapon(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            turnOnSelectedWeapon(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            turnOnSelectedWeapon(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            turnOnSelectedWeapon(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            turnOnSelectedWeapon(4);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            turnOnSelectedWeapon(5);
+        }
+    }
+
+    void turnOnSelectedWeapon(int weaponIndex)
+    {
+        if(current_weapon_index == weaponIndex)
+        {
+            return;
+        }
+
+        weapons[current_weapon_index].gameObject.SetActive(false);
+        weapons[weaponIndex].gameObject.SetActive(true);
+        current_weapon_index = weaponIndex;
+    }
+
+    public WeaponHandler getCurrentSelectedWeapon() //Needed for each individual weapon
+    {
+        return weapons[current_weapon_index];
+    }
+}
