d0593e556404ae5eee9c945e666d5410fb9e1bba
devenfafard <deven.marie.fafard@gmail.com>
2020-05-15 11:41:21 -0700

    Added TextMeshPro assets, configured Main Menu Scene, configured build settings

---
+++ CS-179/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel = null;
+    [SerializeField] private GameObject creditsPanel = null;
+
+    private void Awake()
+    {
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+
+    public void OnPlayClicked()
+    {
+        // Loads Level 1, which is at build index 1.
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void OnCreditsClicked()
+    {
+        creditsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackClicked()
+    {
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+    }
+}
