bbc4ca7d034306164ccf1b85f5477dd0af69ed36
devenfafard <deven.marie.fafard@gmail.com>
2020-05-17 12:27:11 -0700

    Cleaned up tracked files

--- CS-179/Assets/Scripts/UI_Manager.cs
+++ CS-179/Assets/Scripts/UI_Manager.cs
@@ -7,14 +7,43 @@ using UnityEngine.SceneManagement;
 public class UI_Manager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenuPanel = null;
-    [SerializeField] private GameObject creditsPanel = null;
+    [SerializeField] private GameObject creditsPanel  = null;
+    [SerializeField] private GameObject pausePanel    = null;
 
     private void Awake()
     {
-        mainMenuPanel.SetActive(true);
-        creditsPanel.SetActive(false);
+        if (mainMenuPanel.gameObject != null)
+        {
+            mainMenuPanel.SetActive(true);
+        }
+        if (creditsPanel.gameObject != null)
+        {
+            creditsPanel.SetActive(false);
+        }
+        if(pausePanel.gameObject != null)
+        {
+            pausePanel.SetActive(false);
+        }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            // Pause
+            if(!pausePanel.activeInHierarchy)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+            }
+            else
+            {
+                OnResumeClicked();
+            }
+        }
+    }
     public void OnPlayClicked()
     {
         // Loads Level 1, which is at build index 1.
@@ -36,5 +65,13 @@ public class UI_Manager : MonoBehaviour
     {
         mainMenuPanel.SetActive(true);
         creditsPanel.SetActive(false);
+    }
+
+    public void OnResumeClicked()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
